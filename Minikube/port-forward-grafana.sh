9da10d1dd45f4c86867170fe088f3818ee736d31
john detke <jdetke@gmail.com>
2020-06-11 11:28:23 -0700

    port fowarding the service

--- Minikube/port-forward-grafana.sh
+++ Minikube/port-forward-grafana.sh
@@ -2,4 +2,8 @@
 
 ##kubectl port-forward  my-release-grafana-bfb7c8cdf-2wkn9 3001:3000
 #   want to move this to deploy and service, for now manual way:
-kubectl port-forward  my-release-grafana-bfb7c8cdf-2wkn9 3001:3000 >> logfile 2>&1  &
+##kubectl port-forward  my-release-grafana-bfb7c8cdf-2wkn9 3001:3000 >> logfile 2>&1  &
+#
+#   Port forward the service, not the pod
+kubectl port-forward service/my-release-grafana 3000:80 >> logfile 2>&1 &
+
