3aa894c57f1e5dba9d4f90380f500ed3308f4a6e
john detke <jdetke@gmail.com>
2020-06-11 08:57:03 -0700

    manual start, with logging and background

--- Minikube/port-forward-grafana.sh
+++ Minikube/port-forward-grafana.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-kubectl port-forward  my-release-grafana-bfb7c8cdf-2wkn9 3001:3000
+##kubectl port-forward  my-release-grafana-bfb7c8cdf-2wkn9 3001:3000
+#   want to move this to deploy and service, for now manual way:
+kubectl port-forward  my-release-grafana-bfb7c8cdf-2wkn9 3001:3000 >> logfile 2>&1  &
