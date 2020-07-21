7b41590c924ca376b53a3b5113f2002ccf466ff5
john detke <jdetke@gmail.com>
2020-07-21 09:47:28 -0700

    starts pods, next up: deployment

--- Minikube/TerraForm/echoserver/echoserver.tf
+++ Minikube/TerraForm/echoserver/echoserver.tf
@@ -2,14 +2,63 @@ provider "kubernetes" {
   config_context_cluster   = "minikube"
 }
 
-resource "kubernetes_pod" "echoserver1" {
+resource "kubernetes_deployment" "example" {
   metadata {
-    name = "echoserver1"
+    name = "terraform-example"
+    labels = {
+      test = "MyExampleApp"
+    }
   }
+
   spec {
-    container {
-      image = "k8s.gcr.io/echoserver:1.4"
-      name  = "echoserver-name"
+    replicas = 1
+
+    selector {
+      match_labels = {
+        test = "MyExampleApp"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          test = "MyExampleApp"
+        }
+      }
+
+      spec {
+        container {
+          image = "k8s.gcr.io/echoserver:1.4"
+          name  = "example"
+
+          resources {
+            limits {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+
+          //liveness_probe {
+          //  http_get {
+          //    path = "/nginx_status"
+          //    port = 80
+          //
+          //    http_header {
+          //      name  = "X-Custom-Header"
+          //      value = "Awesome"
+          //    }
+          //  }
+          //
+          //  initial_delay_seconds = 3
+          //  period_seconds        = 3
+          //}
+        }
+      }
     }
   }
 }
+
